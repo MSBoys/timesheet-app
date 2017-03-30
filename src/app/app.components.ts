678cf5e794214508f898c507743a03624dd0a525
linkingopi <linkin.go@gmail.com>
2017-03-31 00:13:32 +0200

    my second tryout got result

--- src/app/app.components.ts
+++ src/app/app.components.ts
@@ -1,8 +1,15 @@
 import {Component} from 'angular2/core';
+import {CoursesComponent} from "./courses.component";
+import {AuthorsComponent} from "./authors.components";
 
 @Component({
     selector: 'timesheet-app',
-    template: `<h1>My First App</h1>`
+    template: `
+        <h1>My First App</h1>
+        <courses></courses>
+        <authors></authors>
+    `,
+    directives: [CoursesComponent, AuthorsComponent]
 })
 
 export class AppComponent{}
